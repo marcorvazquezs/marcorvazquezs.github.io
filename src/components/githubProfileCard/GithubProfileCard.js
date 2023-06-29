1ded6a4e06ce1eb32b21aff3dd9f8ae148921040
Marco <marcorvazquez@gmail.com>
2023-06-29 14:07:48 -0700

    deploy

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -6,11 +6,11 @@ import emoji from "react-easy-emoji";
 import {Fade} from "react-reveal";
 
 export default function GithubProfileCard({prof}) {
-  if (isHireable) {
-    prof.hireable = "Yes";
-  } else {
-    prof.hireable = "No";
-  }
+  //if (isHireable) {
+  //  prof.hireable = "Yes";
+  //} else {
+  //  prof.hireable = "No";
+  //}
   return (
     <Fade bottom duration={1000} distance="20px">
       <div className="main" id="contact">
